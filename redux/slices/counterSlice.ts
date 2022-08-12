07032febf52d945c4d2584e32b9667f15d1b5606
Hajar Aswad <hajaraswadkom@gmail.com>
2022-08-12 10:28:53 +0700

    reducer

--- redux/slices/counterSlice.ts
+++ redux/slices/counterSlice.ts
@@ -1,3 +1,23 @@
-import { createAction } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
 
 export const increment = createAction('increment/couter');
+export const decrement = createAction('decrement/couter');
+export const incrementAmount = createAction('incrementAmount/couter');
+
+//reducer
+//1. using builder notation
+
+const initialState={
+    value:0
+};
+export const counterSlice = createReducer(initialState, builder =>{
+    builder.addCase(increment, (state, action)=>{
+        state.value ++;
+    });
+    builder.addCase(decrement, (state, action)=>{
+        state.value--
+    });
+    builder.addCase(incrementAmount, (state, action:any)=>{
+        state.value += action.payload;
+    } );
+})
--- redux/store/store.ts
+++ redux/store/store.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "../slices/counterSlice";
+
 
 const store = configureStore({
     reducer:{
-        counter:{},
+        counter:counterSlice,
     }
 })
 
